7438c8b81b669738a4099707b74febf3748cb0dd
Jose Mugaburu <mugab686@gmail.com>
2017-06-20 00:02:57 -0400

    adds gesture support, theme and material icons reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
